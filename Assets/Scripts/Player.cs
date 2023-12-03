3611eb64ae7377275253bbaacedae82b71673b33
DJSkippy <dajohnson93@gmail.com>
2023-12-02 22:02:15 -0500

    some variable training

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,23 @@
+﻿using JetBrains.Annotations;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+
+    public float speed = 3.5f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // take the current postion = new position (0, 0, 0)
+        transform.position = new Vector3(0, 0, 0);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.Translate(Vector3.right * speed * Time.deltaTime);
+    }
+}
