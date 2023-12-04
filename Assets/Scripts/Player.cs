84550ef901374349e6b36fe5d9a732f2fbf83e28
DJSkippy <dajohnson93@gmail.com>
2023-12-04 14:26:25 -0500

    use input and tip calculator added

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -5,8 +5,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-
-    public float speed = 3.5f;
+    [SerializeField]
+    private float _speed = 3.5f;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +18,12 @@ public class Player : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        transform.Translate(Vector3.right * speed * Time.deltaTime);
+        float horizontalInput = Input.GetAxis("Horizontal");
+        float verticalInput = Input.GetAxis("Vertical");
+        //transform.Translate(Vector3.right * horizontalInput * _speed * Time.deltaTime);
+        //transform.Translate(Vector3.up * verticalInput * _speed * Time.deltaTime);
+        Vector3 direction = new Vector3(horizontalInput, verticalInput, 0);
+        transform.Translate(direction * _speed * Time.deltaTime);
+
     }
 }
